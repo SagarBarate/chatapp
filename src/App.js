e1a7db59254de87c1e5a655eba9f8fb094356fbe
Sagar Barate <sagarbarate2907@com.com>
2023-08-13 23:39:49 +0530

    adding sass file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import { Home } from './pages/Home';
+import { Register } from './pages/Register';
+import "./style.scss"
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <Register/>
     </div>
   );
 }
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,9 +3,17 @@ import React from 'react'
 export const Register = () => {
   return (
     <div className='formContainer'>
-        <div className='formwrap'>
-            
-
+        <div className='formWrapper'>
+            <span className='logo'>Catch Up</span>
+            <span className='title'> Register</span>
+            <form >
+                <input type="text" placeholder='Display name' />
+                <input type="email" placeholder='email'/>
+                <input type="password" placeholder='password' />
+                <input type="file" />
+                <button>Sign up</button>
+            </form>
+            <p> You do have an account? Login</p>
         </div>
     </div>
   )
