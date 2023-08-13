23a5348795e1a08948d205c2f294b1942257b6fd
Sagar Barate <sagarbarate2907@com.com>
2023-08-13 23:25:59 +0530

    first commit

---
+++ src/pages/Register.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Register = () => {
+  return (
+    <div className='formContainer'>
+        <div className='formwrap'>
+            
+
+        </div>
+    </div>
+  )
+}
