3e154653104b75d3b7c78254bde3d1fb4f5e41f8
Sagar Barate <sagarbarate2907@com.com>
2023-08-18 22:44:25 +0530

    commit

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 
 const Message = () => {
   return (
-    <div className='message'>
-      <div className="messageInfo"></div>
-      <div className="messageContent"></div>
+    <div className='message owner'>
+      <div className="messageInfo">
+
+        <img src="https://imgs.search.brave.com/gn43WJFAHYB9uDuVHQNN0yctAEu2CoBMyznn-qstv6Q/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTIw/OTA3ODE5NC9waG90/by9wb3J0cmFpdC1v/Zi1wZW5zaXZlLWFz/aWFuLXlvdW5nLW1h/bi5qcGc_cz02MTJ4/NjEyJnc9MCZrPTIw/JmM9WGpLMzd4dF9L/S2dSVHJXeUdNdGtO/ckkwWmxsMVJBX3g2/UkQyV2p3djhXOD0" alt="" />
+        <span>just now</span>
+      </div>
+
+      <div className="messageContent">
+        <p>hello</p>
+        <img src="https://imgs.search.brave.com/gn43WJFAHYB9uDuVHQNN0yctAEu2CoBMyznn-qstv6Q/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTIw/OTA3ODE5NC9waG90/by9wb3J0cmFpdC1v/Zi1wZW5zaXZlLWFz/aWFuLXlvdW5nLW1h/bi5qcGc_cz02MTJ4/NjEyJnc9MCZrPTIw/JmM9WGpLMzd4dF9L/S2dSVHJXeUdNdGtO/ckkwWmxsMVJBX3g2/UkQyV2p3djhXOD0" alt="" />
+      </div>
     </div>
   )
 }
