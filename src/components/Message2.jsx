6d1c6116d30095723ce3b301ce78501e79dfa533
Sagar Barate <Sagarbarate2907@gmail.com>
2023-08-22 17:02:35 +0530

    Create Message2.jsx

---
+++ src/components/Message2.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Mesaage2 = () => {
+  return (
+    <div className='message user'>
+    <div className="messageInfo">
+
+      <img src="https://imgs.search.brave.com/gn43WJFAHYB9uDuVHQNN0yctAEu2CoBMyznn-qstv6Q/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTIw/OTA3ODE5NC9waG90/by9wb3J0cmFpdC1v/Zi1wZW5zaXZlLWFz/aWFuLXlvdW5nLW1h/bi5qcGc_cz02MTJ4/NjEyJnc9MCZrPTIw/JmM9WGpLMzd4dF9L/S2dSVHJXeUdNdGtO/ckkwWmxsMVJBX3g2/UkQyV2p3djhXOD0" alt="" />
+      <span>just now</span>
+
+    </div>
+
+    <div className="messageContent">
+      <p>Hiiii</p>
+      <p>How was your day....</p>
+    </div>
+  </div>
+  )
+}
+
+export default Mesaage2
