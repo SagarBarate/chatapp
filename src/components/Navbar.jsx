5e26bc72264cbf57e15162fd0b416719f599b49d
Sagar Barate <Sagarbarate2907@gmail.com>
2023-08-22 17:09:09 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import {signOut} from "firebase/auth"
+import { auth } from '../firebase'
+import { AuthContext } from '../context/AuthContext'
 
-export const Navbar = () => {
+const Navbar = () => {
+  const {currentUser} = useContext(AuthContext)
+
   return (
     <div className='navbar'>
-      <span className='logo'> Catch Up</span>
-      <div className='user'>
-        <img src="https://imgs.search.brave.com/taIU1cW7OK6RB9HD8cpcsM2oQFmhQJ1c05IQtePFuUI/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/cHJlbWl1bS1waG90/by9jb25jZXB0LXBl/b3BsZS13aXRoLXlv/dW5nLW1hbi1ibHVl/LWJhY2tncm91bmRf/MTg1MTkzLTgyMDA2/LmpwZz9zaXplPTYy/NiZleHQ9anBn" alt="" />
-        <span>John</span>
-        <button>Logout</button>
+      <span className="logo">Catch UP</span>
+      <div className="user">
+        <img src={currentUser.photoURL} alt="" />
+        <span>{currentUser.displayName}</span>
+        <button onClick={()=>signOut(auth)}>logout</button>
       </div>
-
     </div>
   )
 }
+
+export default Navbar
